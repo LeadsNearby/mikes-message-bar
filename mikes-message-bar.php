5ccd18fa0ee6d51b96054ba1a5b7792df5a89eea
DeliciousLayao <mike@leadsnearby.com>
2017-12-21 15:59:03 -0500

    Update mikes-message-bar.php

--- mikes-message-bar.php
+++ mikes-message-bar.php
@@ -3,7 +3,7 @@
 Plugin URI: http://leadsnearby.com/
 Description: Creates the most amazing fixed message bar in the world without slowing down your site.
 
-Version: 2.0.2
+Version: 2.0.3
 Author: Michael Layao
 Author URI: http://leadsnearby.com/
 License: GPLv2 or later
