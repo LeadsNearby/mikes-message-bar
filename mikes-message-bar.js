24a78a6401ab0b7c0a0762094a50bfe058564712
DeliciousLayao <mike@leadsnearby.com>
2018-01-19 12:44:41 -0500

    Update mikes-message-bar.js

--- mikes-message-bar.js
+++ mikes-message-bar.js
@@ -12,5 +12,17 @@ jQuery(document).ready(function($) {
 					}
 				});
 				$('.fusion-footer').css('padding-bottom', $('#mikes-message-bar').outerHeight() );
-    
+	$(window).on('scroll', function() {
+		if( $(window).scrollTop() === 0 ) {
+			$('#livechat-compact-container').css('bottom', '0px');
+			$('#livechat-eye-catcher').css('bottom', '35px' );
+		}
+		else {
+						$('#livechat-compact-container').css('bottom', '85px');
+						$('#livechat-eye-catcher').css('bottom', '119px');
+					}
+	});
+	$(window).on('scroll load resize', function() {
+		$('#mobile_invitation_container').css('bottom', '85px !important');
+	});
 });
